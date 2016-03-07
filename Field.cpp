6288373549bf7a749ad97e1fe41ba1bdcc5742c5
Sumeet tulsani <stulsani@mail.csuchico.edu>
2016-03-07 11:01:24 -0800

    isSafe function done with unit test cases

--- Field.cpp
+++ Field.cpp
@@ -51,11 +51,22 @@ FieldType Field::get(int x, int y)
  bool Field::isSafe(int x, int y)
  {
  	//TODO: Complete this function, isSafe(int,int)
- 	return true;
+  if(x < 0 || x >= FIELD_DIMENSION || y < 0 || y >= FIELD_DIMENSION)
+  {
+    throw "Out of bounds";
+  }
+  else if(_map[x][y] == MINE_HIDDEN || _map[x][y] == MINE_SHOWN)
+  {
+    return false;
+  }
+  else
+  {
+ 	  return true;
+  }
  }
 
 /**
- * Changes the location from EMPTY_HIDDEN to EMPTY_SHOWN for the 
+ * Changes the location from EMPTY_HIDDEN to EMPTY_SHOWN for the
  * location and any valid locations adjacent (immediately above,
  * below, left, or right as printed) to the provided (x,y) location
 **/
--- FieldTest.cpp
+++ FieldTest.cpp
@@ -5,7 +5,9 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include "Field.h"
- 
+
+using namespace std;
+
 class FieldTest : public ::testing::Test
 {
 	protected:
@@ -18,7 +20,90 @@ class FieldTest : public ::testing::Test
 TEST(FieldTest, placeMineInBounds)
 {
 	Field minefield;
-	
+
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+}
+
+TEST(FieldTest, isSafe)
+{
+	Field minefield;
+  	ASSERT_TRUE( minefield.isSafe(6,5) );
+}
+
+TEST(FieldTest, isSafe1)
+{
+	Field minefield;
+  	ASSERT_TRUE( minefield.isSafe(0,5) );
+}
+
+TEST(FieldTest, isSafe2)
+{
+	Field minefield;
+  	ASSERT_TRUE( minefield.isSafe(6,0) );
+}
+
+TEST(FieldTest, isSafe3)
+{
+	Field minefield;
+  try
+  {
+  	ASSERT_TRUE( minefield.isSafe(69949,56387) );
+  }
+  catch(...)
+  {
+    cout << "Exeption occured" << endl;
+  }
+}
+
+TEST(FieldTest, isSafe4)
+{
+	Field minefield;
+  try
+  {
+  	ASSERT_TRUE( minefield.isSafe(-6,-5) );
+  }
+  catch(...)
+  {
+    cout << "Exeption occured" << endl;
+  }
+}
+
+TEST(FieldTest, isSafe5)
+{
+	Field minefield;
+  try
+  {
+  	ASSERT_TRUE( minefield.isSafe(-6,5) );
+  }
+  catch(...)
+  {
+    cout << "Exeption occured" << endl;
+  }
+}
+
+TEST(FieldTest, isSafe6)
+{
+	Field minefield;
+  try
+  {
+  	ASSERT_TRUE( minefield.isSafe(6,-5) );
+  }
+  catch(...)
+  {
+    cout << "Exeption occured" << endl;
+  }
+}
+
+TEST(FieldTest, isSafe7)
+{
+	Field minefield;
+  try
+  {
+  	ASSERT_FALSE( minefield.isSafe(-6,-5) );
+  }
+  catch(...)
+  {
+    cout << "Exeption occured" << endl;
+  }
 }
